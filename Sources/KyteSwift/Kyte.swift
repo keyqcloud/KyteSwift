7bf12f63faae4ddd05fc3ac4bd514f99b9a7a1e8
Kenneth P. Hough <kenneth@egtech.us>
2022-07-27 13:17:06 -0500

    Simplify and remove NSClassFromString

--- Sources/KyteSwift/Kyte.swift
+++ Sources/KyteSwift/Kyte.swift
@@ -181,12 +181,10 @@ public class Kyte<T>: ObservableObject where T : Codable {
                     
                     completion(sessionData, nil, sessionData.data.sessionToken, sessionData.data.txToken)
                 } else {
-                    let moduleName = Bundle.main.infoDictionary!["CFBundleName"] as! String
-                    let namespace = moduleName.replacingOccurrences(of: "-", with: "_")
-                    let modelClass = NSClassFromString(namespace+"."+model) as AnyObject as? KyteModel<T> ?? KyteModel<T>()
+                    let modelClass = KyteModel<T>()
                     
                     guard let modelData = modelClass.jsonDecode(jsonString: str) else {
-                        print("[Error] " + namespace + "." + model + " not defined")
+                        print("[Error] Unable to parse model \(model)")
                         return
                     }
                     
