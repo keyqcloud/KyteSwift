6774982446df0556d3c99479cecc3fed43bc7d84
Kenneth P. Hough <kenneth@egtech.us>
2022-07-27 17:58:15 -0500

    Allow for endpoint overrides if info.plist isn't avail. Add unit testing

--- Sources/KyteSwift/KyteManager.swift
+++ Sources/KyteSwift/KyteManager.swift
@@ -14,6 +14,30 @@ public class KyteManager: ObservableObject {
         return instance
     }()
 
+    public var endpoint:String?
+    public var publicKey:String?
+    public var secretKey:String?
+    public var accountNumber:String?
+    public var identifier:String?
+    
+    var _endpoint:String {
+        return Bundle.main.object(forInfoDictionaryKey: "KyteEndpointUrl") as? String ?? ""
+    }
+    
+    // endpoint keys
+    var _publicKey:String {
+        return Bundle.main.object(forInfoDictionaryKey: "KytePublicKey") as? String ?? ""
+    }
+    var _secretKey:String {
+        return Bundle.main.object(forInfoDictionaryKey: "KyteSecretKey") as? String ?? ""
+    }
+    var _accountNumber:String {
+        return Bundle.main.object(forInfoDictionaryKey: "KyteAccountNumber") as? String ?? ""
+    }
+    var _identifier:String {
+        return Bundle.main.object(forInfoDictionaryKey: "KyteIdentifier") as? String ?? ""
+    }
+    
     // kyte session store
     private var sessionToken:String = "0"
     private var transactionToken:String = "0"
@@ -22,7 +46,7 @@ public class KyteManager: ObservableObject {
     init() {}
     
     public func destroySession(completion: ((_ data: KyteSessionDataWrapper?, _ error: KyteError?) -> Void)?) {
-        let kyte = Kyte<SessionData>()
+        let kyte = Kyte<SessionData>(withEndpoint: self.endpoint ?? self._endpoint, publickey: self.publicKey ?? self._publicKey, secretkey: self.secretKey ?? self._secretKey, accountnumber: self.accountNumber ?? self._accountNumber, identifier: self.identifier ?? self._identifier)
         kyte.transactionToken = self.transactionToken
         kyte.sessionToken = self.sessionToken
         
@@ -44,7 +68,7 @@ public class KyteManager: ObservableObject {
     }
     
     public func createSession(parameters:[String:Any]?, headers:[String:String]? = nil, completion: ((_ data: KyteSessionDataWrapper?, _ error: KyteError?) -> Void)?) {
-        let kyte = Kyte<SessionData>()
+        let kyte = Kyte<SessionData>(withEndpoint: self.endpoint ?? self._endpoint, publickey: self.publicKey ?? self._publicKey, secretkey: self.secretKey ?? self._secretKey, accountnumber: self.accountNumber ?? self._accountNumber, identifier: self.identifier ?? self._identifier)
         kyte.transactionToken = self.transactionToken
         kyte.sessionToken = self.sessionToken
         
@@ -78,7 +102,7 @@ public class KyteManager: ObservableObject {
     }
     
     public func request<T:Codable>(_ modelDef: T.Type, method: KyteHTTPMethods, model:String, field:String? = nil, value:String? = nil, parameters:[String:Any]? = nil, headers:[String:String]? = nil, completion: @escaping  (_ data: KyteModelDefinition<T>?, _ error: KyteError?) -> Void) {
-        let kyte = Kyte<T>()
+        let kyte = Kyte<T>(withEndpoint: self.endpoint ?? self._endpoint, publickey: self.publicKey ?? self._publicKey, secretkey: self.secretKey ?? self._secretKey, accountnumber: self.accountNumber ?? self._accountNumber, identifier: self.identifier ?? self._identifier)
         kyte.transactionToken = self.transactionToken
         kyte.sessionToken = self.sessionToken
         
--- Tests/KyteSwiftTests/KyteSwiftTests.swift
+++ Tests/KyteSwiftTests/KyteSwiftTests.swift
@@ -6,6 +6,20 @@ final class KyteSwiftTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
+        
+        let k = KyteManager.shared
+        k.endpoint = "https://kyte-example-api.stratis-troika.com"
+        k.secretKey = "8da91895cc9341456f56e0fe21cb089ad883f421"
+        k.publicKey = "130f66286613c6f472ccd26ceb147fe2e854402a"
+        k.accountNumber = "6e068c8f6c259510ff59"
+        k.identifier = "62e1b4bda2d07"
+        
+        k.createSession(parameters: ["email":"info@keyqcloud.com","password":"2*pQ6`,p~~_e7"], headers: nil, completion: {
+            data, error in
+            XCTAssertNotNil(data)
+            XCTAssertNil(error)
+            print("successful login")
+        })
 //        XCTAssertEqual(KyteSwift().text, "Hello, World!")
     }
 }
